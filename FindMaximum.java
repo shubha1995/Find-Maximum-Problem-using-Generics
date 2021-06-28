b141be83fd4a4a136eb4868c813d3af1ef7e55e8
shubhasankar <ssbhaumikdmr@gmail.com>
2021-06-28 21:37:14 +0530

    Added: Welcom to Java Genrics Problems

---
+++ FindMaximum.java
@@ -0,0 +1,7 @@
+package com.genrics;
+
+public class FindMaximum {
+	    public static void main(String[] args) {
+	        System.out.println("Welcome to Find Maximum Problem using Generics");
+	 }
+}
