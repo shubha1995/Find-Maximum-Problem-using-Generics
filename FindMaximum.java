32e4349fa9c62a011e122764a9697588d164918c
shubhasankar <ssbhaumikdmr@gmail.com>
2021-06-28 21:40:40 +0530

    Added: Give 3 Integers and find the maximum

--- FindMaximum.java
+++ FindMaximum.java
@@ -3,5 +3,23 @@ package com.genrics;
 public class FindMaximum {
 	    public static void main(String[] args) {
 	        System.out.println("Welcome to Find Maximum Problem using Generics");
-	 }
-}
+	        Integer a = 20, b = 50, c =80;
+	        testMaximum (a,b,c);
+	    }
+
+	    private static void testMaximum(Integer a, Integer b, Integer c) {
+	        Integer max = a;
+	        if(b.compareTo(a)>0){
+	            max = b;
+	        }
+	        if(c.compareTo(max)>0){
+	            max = c;
+	        }
+	        System.out.println("Maximum Number is::"+max);
+	    }
+
+
+	}
+
+
+
