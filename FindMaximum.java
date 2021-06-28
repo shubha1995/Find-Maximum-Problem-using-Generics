e822539c6e51f630a1af56461881d81e34ed9634
shubhasankar <ssbhaumikdmr@gmail.com>
2021-06-29 00:59:58 +0530

    Added: Take_Three_Float_Value_and_Find_Maximum

--- FindMaximum.java
+++ FindMaximum.java
@@ -3,17 +3,17 @@ package com.genrics;
 public class FindMaximum {
 	    public static void main(String[] args) {
 	        System.out.println("Welcome to Find Maximum Problem using Generics");
-	        String e = "Bangalore" , f = "GOa" , g = "Mumbai";
-	        testMaximum (e,f,g);
+	        Float p = 20.85f, q = 90.55f, r =80.98f;
+	        testMaximum (p,q,r);
 	    }
 
-	    private static void testMaximum(String e, String f, String g) {
-	    	 String max = e;
-		        if(f.compareTo(e)>0){
-		            max = f;
+	    private static void testMaximum(Float p, Float q, Float r) {
+	    	 Float max = p;
+		        if(q.compareTo(p)>0){
+		            max = q;
 		        }
-		        if(g.compareTo(max)>0){
-		            max = g;
+		        if(r.compareTo(max)>0){
+		            max = r;
 		        }
 		        System.out.println("Maximum Number is: "+max);
 	    	
